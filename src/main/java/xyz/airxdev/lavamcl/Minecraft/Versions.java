b2c3c72c90d2a257d94e5f1a313c7c17ea709d46
ZRY551 <zryyou163@163.com>
2022-03-13 21:03:44 +0800

    初架成-3.5-开发ing

--- src/main/java/xyz/airxdev/lavamcl/Minecraft/Versions.java
+++ src/main/java/xyz/airxdev/lavamcl/Minecraft/Versions.java
@@ -1,7 +1,10 @@
 package xyz.airxdev.lavamcl.Minecraft;
 
+
+import com.alibaba.fastjson.JSONObject;
 import xyz.airxdev.lavamcl.Downloader.GetPost;
 import xyz.airxdev.lavamcl.Main;
+import xyz.airxdev.lavamcl.Tools.B64T;
 
 public class Versions {
     //public static String
@@ -10,7 +13,12 @@ public class Versions {
     public static String Type_Old_Alpha = "old_alpha";
     public static String Type_Old_Beta = "old_beta";
     public static String GetVersionList(String Type){
-        return GetPost.Get(Main.DownloadServer[0]);
-        //return "";
+        String AD = GetPost.Get(Main.DownloadServer[0]);
+        JSONObject JS = new JSONObject();
+        try{
+            return "";
+        }catch (Exception ex){
+            return "ERROR\n" + B64T.Encode(ex.toString());
+        }
     }
 }
